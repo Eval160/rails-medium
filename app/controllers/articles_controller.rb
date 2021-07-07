f4f90eff97c0f2ca66e231db17da5395f56bff47
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2021-07-07 16:14:13 +0200

    [FEAT] add article controller and view show and index

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,13 @@
+class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comments = @article.comments
+    @new_comment = Comment.new
+  end
+end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :comments
   validates :title, presence: true, length: { minimum: 3 }
   validates :content, presence: true, length: { minimum: 10 }
   validates :publish, presence: true
