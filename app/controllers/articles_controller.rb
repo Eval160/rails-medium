3e44c7254b4e3cc5989582c76990721aa24111f7
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2021-07-09 23:27:50 +0200

    [FEAT] add search filter in navbav -> filter in articles' title

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,51 @@
+class ArticlesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def index
+    @articles = Article.where(publish: true).order(created_at: :desc).with_rich_text_rich_content_and_embeds
+    search = params["search"]
+    if search.present?
+      title = search["title"]
+      @articles = Article.where("title ILIKE ?", "%#{title}%", publish: true).order(created_at: :desc).with_rich_text_rich_content_and_embeds
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+    @comments = @article.comments
+    @new_comment = Comment.new
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.user = current_user
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.publish = true
+    if @article.save
+      redirect_to root_path
+    end
+  end
+
+  def unpublished
+    @articles = Article.where(publish: false, user: current_user)
+  end
+
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :rich_content, :publish)
+  end
+end
