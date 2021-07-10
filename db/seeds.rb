4effd56fa2e6af06038370e03986e4987559a208
Valentine Ecrepont <ecrepontvalentine@hotmail.fr>
2021-07-10 22:20:22 +0200

    [FEAT]add gem faker and complete seed with more users, articles, comments, and add vote

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
 puts "Delete all comments"
 Comment.destroy_all
 
-puts "Delete all vote"
+puts "Delete all votes"
 Vote.destroy_all
 
 puts "Delete all articles"
@@ -18,16 +19,45 @@ Article.destroy_all
 puts "Delete all users"
 User.destroy_all
 
-puts "Create users Riri, Fifi and Loulou"
+puts "Create the dream team: users Riri, Fifi and Loulou"
 riri = User.create! nickname: "Riri", email: "riri@gmail.com", password: "azerty"
 fifi = User.create! nickname: "Fifi", email: "fifi@gmail.com", password: "azerty"
 loulou = User.create! nickname: "Loulou", email: "loulou@gmail.com", password: "azerty"
 
+puts "Try to populate this world a little bit: create random users"
+for i in (1..10) do
+  User.create! nickname: Faker::TvShows::RickAndMorty.character , email: Faker::Internet.email, password: "azerty"
+end
+
 puts "Create articles"
-article_1 = Article.create! title: "Mon premier article", rich_content: "Cet article est vraiment pationnant, lisez le jusqu'au bout!", user: riri, publish: true
+article_1 = Article.create! title: "Mon premier article", rich_content: "Cet article est vraiment pationnant, lisez le jusqu'au bout! Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user: riri, publish: true
 article_2 = Article.create! title: "Mon deuxieme article", rich_content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", user: fifi, publish: true
 
-puts "Create comments"
+for i in (1..20) do
+  title = Faker::Book.title
+  content = Faker::Lorem.paragraph(sentence_count: 40, supplemental: true, random_sentences_to_add: 4)
+  user = User.all.sample
+  Article.create! title: title, rich_content: content, user: user, publish: true
+end
 
+puts "Create comments"
 comment_1 = Comment.create! content: "Wouah super article, il m'a vraiment fait reflechir au sens de la vie", article: article_1, user: fifi
 comment_2 = Comment.create! content: "Bof, j'ai deja lu plus interessant", article: article_2, user: loulou
+
+for i in (1..60) do
+  content = Faker::Lorem.paragraph(sentence_count: 2, supplemental: true, random_sentences_to_add: 4)
+  article = Article.all.sample
+  user = User.all.sample
+  Comment.create! content: content, article: article, user: user
+end
+
+
+puts "Add some upvote"
+
+for i in (1..100) do
+  user = User.all.sample
+  article = Article.all.sample
+  Vote.create user: user, article: article
+end
+
+puts "Done!"
